7377cc2daefdcd8f42439ed7651b0aa5d279d979
Jeroen Hoolmans <jhoolmans@gmail.com>
2015-01-25 17:01:11 +0100

    Base implementation of Reader class.

---
+++ ambassadors/openclips/reader.py
@@ -0,0 +1,12 @@
+from xml.dom.minidom import getDOMImplementation
+
+class Reader(object):
+    # constructor
+    def __init__(self):
+        pass 
+    #end __init__
+
+    def from_file(self, path):
+        
+    #end read_clip
+#end Reader
